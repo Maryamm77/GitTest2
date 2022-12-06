70472c591c1a7ae6a52fe5561504323ae3a34ad7
Maria <maldeamaria@yahoo.com>
2022-12-06 21:43:31 +0200

    vers2

--- src/MyClass.java
+++ src/MyClass.java
@@ -1,5 +1,10 @@
 public class MyClass {
     public static void main(String[] args) {
         System.out.println("test git");
+        System.out.println("func 2");
+        int i;
+        int k;
+
+
     }
 }
